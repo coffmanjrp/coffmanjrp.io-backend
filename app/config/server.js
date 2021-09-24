e8d265ee3be29365fa3ed1681b031252cef56b50
coffmanjrp <coffmanjrp@gmail.com>
2021-09-23 18:52:06 -0700

    Setup starpi and install strapi-provider-upload-cloudinary

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3e37ae37b87730161c8ba9a2506702c3'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '397a937d-6a1d-4f81-8812-b51eddc92b30'
+};
