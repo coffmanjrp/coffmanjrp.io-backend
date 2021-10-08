c0341ef34c37165e6a65cdc5396d370bf261703d
coffmanjrp <coffmanjrp@gmail.com>
2021-10-08 16:57:06 -0700

    Install strapi graphql plugin

--- app/config/plugins.js
+++ app/config/plugins.js
@@ -13,4 +13,14 @@ module.exports = ({ env }) => ({
       delete: {},
     },
   },
+  graphql: {
+    endpoint: "/graphql",
+    shadowCRUD: true,
+    playgroundAlways: false,
+    depthLimit: 7,
+    amountLimit: 100,
+    apolloServer: {
+      tracing: false,
+    },
+  },
 });
